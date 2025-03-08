455c5251b42ae8e1e3076d108f436a16691bdba1
RekZNoZ <rafikicroz@hotmail.com>
2025-03-08 15:49:40 +0100

    Create getNumeroRandom.js

---
+++ src/helper/getNumeroRandom.js
@@ -0,0 +1,11 @@
+
+// funcion que simula una promesa que se resuelve con un numero random
+const getNumeroRandom = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Math.floor(Math.random() * 10))
+        }, 2000)
+    })
+}
+
+export default getNumeroRandom
