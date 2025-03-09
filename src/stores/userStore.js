ca1b90e92788253ae71b277944564da5dab90c90
RekZNoZ <rafikicroz@hotmail.com>
2025-03-09 19:07:23 +0100

    Update userStore.js

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,55 +1,46 @@
-import {defineStore, mapState} from "pinia";
-import {logoutAuth} from "@/helper/getUsuarioAuth.js";
+import { defineStore } from "pinia"
 
-/*
+const userStore = defineStore("userStore", {
+    state: () => ({
+        user: null,
+        isLoggedIn: false,
+        token: null
+    }),
 
-{
-	"access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YxL2F1dGgvbG9naW4iLCJpYXQiOjE3NDE0NDE2ODUsImV4cCI6MTc0MTQ0NTI4NSwibmJmIjoxNzQxNDQxNjg1LCJqdGkiOiJrZHJ4bzQ3OEtmdkE5Z0M4Iiwic3ViIjoiMSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ._T9LJjD3Eh9S0XL08sXJji20f7uQVnK1B0J8PA6By8w",
-	"token_type": "bearer",
-	"expires_in": 3600,
-	"user": {
-		"id": 1,
-		"name": "Admin",
-		"email": "admin@admin.com",
-		"roles": [],
-		"permissions": []
-	}
-}
+    actions: {
+        login({ user, access_token }) {
+            if (!access_token) return
 
-*/
-
-const userStore = defineStore('userStore', {
-        state: () => ({
-            user: {},
-            isLoggedIn: false
-        }),
-
-        actions: {
-            login(user) {
-                this.isLoggedIn = true
-                this.user = user
-            },
-            logout() {
-                /*
-                logoutAuth().then(r => {
-                        this.isLoggedIn = false
-                        this.user = {}
-                    }
-                )
-                */
-                this.isLoggedIn = false
-                this.user = {}
-            },
+            this.isLoggedIn = true
+            this.user = user
+            this.token = access_token
         },
 
-        persist: [
-            {
-                key: 'userStore',
-                storage: localStorage
+        logout() {
+            this.isLoggedIn = false
+            this.user = null
+            this.token = null
+            localStorage.removeItem("userStore")
+        },
+
+        checkTokenValidity() {
+            const stored = JSON.parse(localStorage.getItem("userStore"))
+            if (stored && stored.token) {
+                const tokenData = JSON.parse(atob(stored.token.split('.')[1]))
+                const isExpired = tokenData.exp * 1000 < Date.now()
+                if (isExpired) {
+                    this.logout()
+                } else {
+                    this.login(stored)
+                }
             }
-        ]
+        }
+    },
 
+    persist: {
+        key: "userStore",
+        storage: localStorage
     }
-)
+})
 
-export default userStore;
+export default userStore
