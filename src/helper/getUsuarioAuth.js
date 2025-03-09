bf46d14caf81fa8e34e3a22f5c0862db9bc744da
RekZNoZ <rafikicroz@hotmail.com>
2025-03-09 03:22:08 +0100

    Update getUsuarioAuth.js

--- src/helper/getUsuarioAuth.js
+++ src/helper/getUsuarioAuth.js
@@ -31,3 +31,13 @@ export const getUsuarioAuth = async (email, password) => {
     return await res.json();
 
 };
+
+export const logoutAuth = async () => {
+    const res = await fetch('http://127.0.0.1:8000/api/v1/auth/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    );
+}
