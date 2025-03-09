daea30fc9b477de08d2a1db815cde53388915977
RekZNoZ <rafikicroz@hotmail.com>
2025-03-09 21:17:40 +0100

    Update crearTarea.js

--- src/helper/crearTarea.js
+++ src/helper/crearTarea.js
@@ -1,15 +1,27 @@
+import userStore from "@/stores/userStore.js";
 
 export const crearTarea = async (tarea) => {
-    const res = await fetch(import.meta.env.VITE_API_URL_TAREAS, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(tarea),
-    });
-    const data = await res.json();
-    if (res.status !== 201) {
-        throw new Error(data.message);
+    try {
+        const res = await fetch(import.meta.env.VITE_API_URL_TAREAS, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // Barier token
+                'Authorization': `Bearer ${userStore.access_token}`
+            },
+            body: JSON.stringify(tarea),
+        });
+
+        if (!res.ok) {
+            const text = await res.text();  // Obtener el contenido como texto
+            console.error("Error en la respuesta del servidor:", text);
+            return
+        }
+
+        // Intentar obtener JSON si la respuesta es correcta
+        return await res.json();
+    } catch (error) {
+        console.error("Error en crearTarea:", error);
+        throw new Error(error.message || 'Error desconocido');
     }
-    return data;
 }
