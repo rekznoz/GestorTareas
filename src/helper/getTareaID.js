b0c534a02e2a9d56839342d118d6e7502f83048b
RekZNoZ <rafikicroz@hotmail.com>
2025-03-09 17:56:39 +0100

    Update getTareaID.js

--- src/helper/getTareaID.js
+++ src/helper/getTareaID.js
@@ -3,7 +3,7 @@
 export const getTareaID = async (id) => {
     if (!id) throw new Error("El ID de usuario es requerido");
 
-    const res = await fetch(`http://127.0.0.1:8000/api/v1/tareas/${id}`);
+    const res = await fetch(`${import.meta.env.VITE_API_URL_TAREAS}/${id}`);
     const data = await res.json();
 
     if (!data) throw new Error("No se encontraron tareas");
