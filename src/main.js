0e89e213b50826c00caae855a38072fe12a771bd
RekZNoZ <rafikicroz@hotmail.com>
2025-03-09 20:56:56 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/router.js";
 import {createPinia} from "pinia";
+import userStore from "@/stores/userStore.js";
 
 const pinia = createPinia()
+const store = userStore(pinia);
+store.checkTokenValidity();
 
 createApp(App).use(router).use(pinia).mount('#app')
