6d70cd0e5bfe214987feb36d1b46e662084e5a16
RekZNoZ <rafikicroz@hotmail.com>
2025-03-11 05:50:48 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import {defineComponent} from "vue";
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default defineComponent({
   components: {Footer, Header}
