587aee911f3201f739cf6e4303adaf7ac60f5dc7
RekZNoZ <rafikicroz@hotmail.com>
2025-03-13 23:43:35 +0100

    Update obtenerPaginacion.js

--- src/helper/obtenerPaginacion.js
+++ src/helper/obtenerPaginacion.js
@@ -1,3 +1,10 @@
+/**
+ * Función que obtiene las tareas de la página actual.
+ * @param tareas
+ * @param paginaActual
+ * @param tareasPorPagina
+ * @returns {*}
+ */
 export const obtenerPaginacion = (tareas, paginaActual, tareasPorPagina) => {
     const inicio = (paginaActual - 1) * tareasPorPagina;
     const fin = inicio + tareasPorPagina;
