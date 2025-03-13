5e6f242d702affe8efd87f9ba4b2a0f7e17e4e75
RekZNoZ <rafikicroz@hotmail.com>
2025-03-13 23:43:15 +0100

    Update eliminarComentario.js

--- src/helper/eliminarComentario.js
+++ src/helper/eliminarComentario.js
@@ -1,5 +1,10 @@
 import userStore from "@/stores/userStore.js";
 
+/**
+ * Elimina un comentario de la base de datos por su id
+ * @param idComentario
+ * @returns {Promise<any>}
+ */
 const eliminarComentario = async (idComentario) => {
     try {
         const res = await fetch(`${import.meta.env.VITE_API_URL_COMENTARIOS}/${idComentario}`, {
