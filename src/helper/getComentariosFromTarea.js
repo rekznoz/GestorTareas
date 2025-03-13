34174f3aa7ee328fb37adb00517ce7cc35c7720d
RekZNoZ <rafikicroz@hotmail.com>
2025-03-13 18:28:47 +0100

    Update getComentariosFromTarea.js

--- src/helper/getComentariosFromTarea.js
+++ src/helper/getComentariosFromTarea.js
@@ -10,20 +10,11 @@ const getComentarioFromTarea = async (id) => {
         throw new Error("La respuesta de la API no es una lista de tareas");
     }
 
-    /*
-    {
-      "id": 1955,
-      "comentario": "Molestias quis in laudantium quae ex vero. Voluptatum quidem eaque repellendus voluptas praesentium aperiam. Esse molestias eius distinctio impedit.",
-      "tarea_id": 196,
-      "user_id": 1966
-    },
-    */
-
     return data.map(comentario => ({
         id: comentario.id,
         comentario: comentario.comentario,
-        tarea_id: comentario.tarea_id,
-        user_id: comentario.user_id,
+        tarea: comentario.tarea,
+        user: comentario.user,
     }));
 }
 
