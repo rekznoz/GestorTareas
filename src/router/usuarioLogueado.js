fe5425cb626cb7c9cfca946051ac00fbf315100f
RekZNoZ <rafikicroz@hotmail.com>
2025-03-13 23:43:27 +0100

    Update usuarioLogueado.js

--- src/router/usuarioLogueado.js
+++ src/router/usuarioLogueado.js
@@ -1,5 +1,11 @@
 import userStore from "@/stores/userStore.js";
 
+/**
+ * Verifica si el usuario está logueado
+ * @param to
+ * @param from
+ * @param next
+ */
 export const usuarioLogueado = (to, from, next) => {
     if (userStore().isLoggedIn) {
         next();
@@ -8,6 +14,12 @@ export const usuarioLogueado = (to, from, next) => {
     }
 };
 
+/**
+ * Verifica si el usuario no está logueado
+ * @param to
+ * @param from
+ * @param next
+ */
 export const usuarioNoLogueado = (to, from, next) => {
     if (!userStore().isLoggedIn) {
         next();
