aca222225da1749deee915f6b8cde3a8583316ac
RekZNoZ <rafikicroz@hotmail.com>
2025-03-13 22:59:51 +0100

    Create eliminarComentario.js

---
+++ src/helper/eliminarComentario.js
@@ -0,0 +1,21 @@
+import userStore from "@/stores/userStore.js";
+
+const eliminarComentario = async (idComentario) => {
+    try {
+        const res = await fetch(`${import.meta.env.VITE_API_URL_COMENTARIOS}/${idComentario}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': `Bearer ${userStore().access_token}`
+            },
+        });
+
+        return await res.json();
+
+    } catch (error) {
+        console.error("Error al crear comentario:", error);
+        throw new Error(error.message || "Error inesperado en la creación del comentario");
+    }
+}
+
+export default eliminarComentario;
